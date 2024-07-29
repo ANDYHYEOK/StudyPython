1a5e677bed8f88cecf13dafe17befaa9fab7f66c
hyunvv00 <chohyunwoo23@gmail.com>
2024-07-29 22:41:53 +0900

    Update example2.py

--- part1/example2.py
+++ part1/example2.py
@@ -3,64 +3,50 @@
 # 구조적으로 클래스를 만들만한 상황을 클래스로 만들어보자!
 
 # 격투기 선수 두명 김동현 vs 은가누 <= 격투기선수()
-import random
 
-class FighterClass:
+import random
+--------------------------------------------------
+class fighterClass:
   
     def __init__(self):
         pass
 
-    def kim_punch(self, x, y):
+    def userInput(self):
+        userName = input("user name: ")
+        return userName
+
+    def statusInput(self):
+        h = float(input("height: "))
+        w = float(input("weight: "))
+        return h + w
+
+    def attack(self, x):
+        y = random.randint(1, 100)
         return x - y
-
-    def kim_kick(self, x, y):
-        return x - y
-
-    def sliver_punch(self, x, y):
-        return x - (3 * y)
-
-    def sliver_kick(self, x, y):
-        return x - y
-
+--------------------------------------------------
 def main():
-    fight = FighterClass()
-    
-    print("Select operation:")
-    print("1. 김동현 주먹!")
-    print("2. 김동현 발차기")
-    print("3. 은가누 주먹!")
-    print("4. 은가누 발차기!")
-
-    num1 = 100
-    num2 = 100
+    f = fighterClass()
+  
+    print("user status: ")
+    username = f.userInput
+    userlife = f.statusInput
+  
+    print("enemy status: ")
+    enemyname = f.userInput
+    enemylife = f.statusInput
 
     while True:
-        choice = input("Enter choice (1/2/3/4): ")
-        num3 = random.randint(1, 10)
-        if choice in ['1', '2', '3', '4']:
-            if 1 < num1 :
-                if choice == '1':
-                    print(f"Damage: {num3} sliver life: {fight.kim_punch(num2, num3)}")
-                elif choice == '2':
-                    print(f"Damage: {num3} sliver life: {fight.kim_kick(num2, num3)}")
-                elif choice == '3':
-                    print(f"Damage: {3 * num3} kim life: {fight.sliver_punch(num1, num3)}")
-                elif choice == '4':
-                    print(f"Damage: {num3} kim life: {fight.sliver_kick(num1, num3)}")
-            else :
-                if num1 < num2 :
-                    print("--sliver win--")
-
-                else :
-                    print("--kim win--")
-
+        user = f.attack(0)
+        enemy = f.attack(0)
+        if user < enemy:
+            print(f"damage: {enemy * (-1)}")
+            print(f"user life: {f.attack(userlife)}")
         else:
-            print("Invalid choice! Please select from 1, 2, 3, or 4.")
-
-        num4 = input("Do you want to perform another calculation? (yes/no): ")
-        
-        if num4.lower() == 'no':
+            print(f"damage: {user * (-1)}")
+            print(f"enemy life: {f.attack(enemylife)}")
+        c = input("continue? (yes/no): ")
+        if c.lower() == 'no':
             break
-
+--------------------------------------------------
 if __name__ == "__main__":
     main()
